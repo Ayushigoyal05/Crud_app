40e774fccf8d38f38e06ba719746cb0baf5f3d50
Ayushi Goyal <ashigoyal05@gmail.com>
2020-07-14 00:37:24 +0530

    model

--- crud_app/crud/models.py
+++ crud_app/crud/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Emp(models.Model):
+    name=models.CharField(max_length=100,null=False)
+    role=models.CharField(max_length=150,null=False)
+    phone=models.IntegerField()
+    address=models.CharField(max_length=250,null=False)
+    salary=models.IntegerField()
+
+
+    def __str__(self):
+        return str(self.name)+" "+str(self.role)
